9d3f18f1276e3420e757a8de6a9b85fef2034fe7
ydynmukhamed <dynmukhamed.ye@chocofood.kz>
2020-02-10 17:14:41 +0600

    Sparse table

--- sparse_table_rmq.py
+++ sparse_table_rmq.py
@@ -4,10 +4,30 @@ from math import log2
 class SparseTableRMQ:
     def __init__(self, arr):
         n = len(arr)
-        self.precomputed = [[0 for i in range(n)] for j in range(n)]
+        self.preprocessed = [[0 for i in range(n)] for j in range(int(log2(n)) + 1)]
 
         for i in range(n):
-            self.precomputed[0][i] = arr[i]
+            self.preprocessed[0][i] = arr[i]
 
-        for j in range(1, log2(n)):
-            pass
+        for j in range(1, int(log2(n)) + 1):
+            i = 0
+            while (i + 2 ** (j - 1)) < n:
+                self.preprocessed[j][i] = min(
+                    self.preprocessed[j - 1][i], self.preprocessed[j - 1][i + 2 ** (j - 1)]
+                )
+                i += 1
+
+    def query(self, i, j):
+        n = j - i
+        k = int(log2(n))
+        return min(
+            self.preprocessed[k][i], self.preprocessed[k][j - 2 ** k]
+        )
+
+
+a = [3, 1, 5, 3, 4, 7, 6]
+rmq = SparseTableRMQ(a)
+for i in rmq.preprocessed:
+    print(i)
+
+print(rmq.query(2, len(a)))
